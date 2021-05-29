d7930a23ce726afcc69534fe15cf5c0da0535f70
fernandafons <nanda.fonseca7@gmail.com>
2021-05-28 23:52:10 -0300

    add time in contdown js file

--- src/features/timer/timer.js
+++ src/features/timer/timer.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 import {colors} from '../../utils/color';
 import {spacing} from '../../utils/sizes';
+import { Countdown } from '../../components/CountDown';
 
 export const Timer = ({focusSubject}) => {
   return (
     <View style={styles.container}>
+      <Countdown />
     <View style={{paddingTop: spacing.xxl}}>
     <Text style={styles.title} >Focusing on: </Text>
       <Text style={styles.task}>{focusSubject}</Text>
